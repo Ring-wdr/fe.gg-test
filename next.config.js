85d63f7262a3b978ac3162c38745f8f8080245cc
Ring-wdr <akswnd55@gmail.com>
2022-12-15 23:37:25 +0900

    feat: add champ img randomly

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    minimumCacheTTL: 60,
+    domains: ['ddragon.leagueoflegends.com'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,40 @@
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
 import styles from './css/index.module.css';
-
+// interface ICham {
+// id: string;
+// name: string;
+// image: { full: string };
+// }
 export default function Home() {
+  const [champList, setChampList] = useState<string[]>([]);
+
+  useEffect(() => {
+    const ctl = new AbortController();
+    const { signal } = ctl;
+    (async () => {
+      const { data } = await fetch(
+        `http://ddragon.leagueoflegends.com/cdn/12.23.1/data/ko_KR/champion.json`,
+        { signal }
+      ).then((res) => res.json());
+      setChampList(Object.keys(data));
+    })();
+    return () => ctl.abort();
+  }, []);
   return (
     <div className={styles['fe-gg']}>
+      <div className={styles.champimg}>
+        {champList.length > 0 ? (
+          <Image
+            src={`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${
+              champList[parseInt(String(champList.length * Math.random() - 1))]
+            }_0.jpg`}
+            alt='randchamp'
+            width={200}
+            height={400}
+          />
+        ) : null}
+      </div>
       <div>
         <Image
           className={styles.logo}
